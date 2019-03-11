4ca2fb3141594ea4ac19d49dab1b9d32f6dcd887
Tabeth Nkangoh <tabeth@tabeth.com>
2019-03-11 12:05:30 -0400

    Add environment variable for PORT

--- server.py
+++ server.py
@@ -1,3 +1,10 @@
 from waitress import serve
 import app
-serve(app.app, host="0.0.0.0", port=8005)
+import os 
+
+try:
+    port = os.environ["HTTP_ODBC_PORT"]
+except:
+    port = 8005
+
+serve(app.app, host="0.0.0.0", port=port)
